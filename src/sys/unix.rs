70efe5c791ba545a264cb40af857360f2cdd429b
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-08-17 12:31:16 +0200

    Unify sigpipe settings for macOS and iOS
    Original commit: 7bf31f95af1905880e73a169b0eb3894597e6f0f

--- src/sys/unix.rs
+++ src/sys/unix.rs
@@ -188,7 +188,7 @@ impl Socket {
             let fd = cvt(libc::socket(family, ty, protocol))?;
             let fd = Socket::from_raw_fd(fd);
             set_cloexec(fd.as_raw_fd())?;
-            #[cfg(target_os = "macos")]
+            #[cfg(any(target_os = "macos", target_os = "ios"))]
             {
                 fd.setsockopt(libc::SOL_SOCKET, libc::SO_NOSIGPIPE, 1i32)?;
             }
@@ -203,7 +203,7 @@ impl Socket {
             let fds = (Socket::from_raw_fd(fds[0]), Socket::from_raw_fd(fds[1]));
             set_cloexec(fds.0.as_raw_fd())?;
             set_cloexec(fds.1.as_raw_fd())?;
-            #[cfg(target_os = "macos")]
+            #[cfg(any(target_os = "macos", target_os = "ios"))]
             {
                 fds.0
                     .setsockopt(libc::SOL_SOCKET, libc::SO_NOSIGPIPE, 1i32)?;
