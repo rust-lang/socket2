6994498718af168f341d920f7b7f742f56f9b3b3
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-12-18 13:53:52 +0100

    Ignore some some option tests on Windows

--- tests/options.rs
+++ tests/options.rs
@@ -87,7 +87,11 @@ test!(
 );
 
 test!(IPv4 ttl, set_ttl(40));
+#[cfg(not(windows))] // TODO: returns `WSAENOPROTOOPT` (10042) on Windows.
 test!(IPv4 broadcast, set_broadcast(true));
 
 test!(IPv6 unicast_hops_v6, set_unicast_hops_v6(20));
+#[cfg(not(windows))]
 test!(IPv6 only_v6, set_only_v6(true));
+#[cfg(windows)] // IPv6 socket are already IPv6 only on Windows.
+test!(IPv6 only_v6, set_only_v6(false));
