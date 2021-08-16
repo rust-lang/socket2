6c261dc4b43502a721d39cd47424747d6ffc594d
Craig McQueen <craig.mcqueen@innerrange.com>
2021-08-17 09:51:04 +1000

    Example doc comment: fix usage of socket.set_only_v6()
    The call to socket.set_only_v6() needs to be *before* the call to
    socket.bind().

--- src/lib.rs
+++ src/lib.rs
@@ -34,9 +34,9 @@
 //! // Create a TCP listener bound to two addresses.
 //! let socket = Socket::new(Domain::IPV6, Type::STREAM, None)?;
 //!
+//! socket.set_only_v6(false)?;
 //! let address: SocketAddr = "[::1]:12345".parse().unwrap();
 //! socket.bind(&address.into())?;
-//! socket.set_only_v6(false)?;
 //! socket.listen(128)?;
 //!
 //! let listener: TcpListener = socket.into();
