6066f887b747d530d57a76fd768ba00a3eb478d6
Niklas Hallqvist <niklas+github@appli.se>
2022-01-28 15:23:01 +0100

    Update socket.rs

--- src/socket.rs
+++ src/socket.rs
@@ -1135,6 +1135,7 @@ impl Socket {
         target_os = "haiku",
         target_os = "illumos",
         target_os = "netbsd",
+        target_os = "openbsd",
         target_os = "redox",
         target_os = "solaris",
     )))]
