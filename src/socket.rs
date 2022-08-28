5b995a0f67629b5273c7d12349acdc1c10093c1c
Volodymyr Kostyrko <arcade@b1t.name>
2022-08-28 17:27:58 +0300

    also properly disable SSM for DragonFly

--- src/socket.rs
+++ src/socket.rs
@@ -1192,6 +1192,7 @@ impl Socket {
     /// multicast group. If it's [`Ipv4Addr::UNSPECIFIED`] (`INADDR_ANY`) then
     /// an appropriate interface is chosen by the system.
     #[cfg(not(any(
+        target_os = "dragonfly",
         target_os = "haiku",
         target_os = "netbsd",
         target_os = "redox",
@@ -1224,6 +1225,7 @@ impl Socket {
     ///
     /// [`join_ssm_v4`]: Socket::join_ssm_v4
     #[cfg(not(any(
+        target_os = "dragonfly",
         target_os = "haiku",
         target_os = "netbsd",
         target_os = "redox",
