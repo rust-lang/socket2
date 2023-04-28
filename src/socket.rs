7b5d8e159700df3f60b9b981e9d1ff101da9170d
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2023-04-28 05:29:43 +0000

    Haiku: fix build as IP_RECVTOS does not exist

--- src/socket.rs
+++ src/socket.rs
@@ -1432,6 +1432,7 @@ impl Socket {
     #[cfg(not(any(
         target_os = "dragonfly",
         target_os = "fuchsia",
+        target_os = "haiku",
         target_os = "illumos",
         target_os = "netbsd",
         target_os = "openbsd",
@@ -1461,6 +1462,7 @@ impl Socket {
     #[cfg(not(any(
         target_os = "dragonfly",
         target_os = "fuchsia",
+        target_os = "haiku",
         target_os = "illumos",
         target_os = "netbsd",
         target_os = "openbsd",
