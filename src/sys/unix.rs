c93d95a0140f092ca1a7102ff7834cade5f74051
svante karlsson <svante.karlsson@csi.se>
2026-05-05 12:26:30 +0200

    Split target_os = "nto" cfg by env so io-sock variants use TCP_KEEPIDLE (#654)

--- src/sys/unix.rs
+++ src/sys/unix.rs
@@ -324,7 +324,7 @@ pub(crate) type Bool = c_int;
     target_os = "ios",
     target_os = "visionos",
     target_os = "macos",
-    target_os = "nto",
+    all(target_os = "nto", any(target_env = "nto70", target_env = "nto71"),),
     target_os = "tvos",
     target_os = "watchos",
 ))]
@@ -334,7 +334,7 @@ use libc::TCP_KEEPALIVE as KEEPALIVE_TIME;
     target_os = "ios",
     target_os = "visionos",
     target_os = "macos",
-    target_os = "nto",
+    all(target_os = "nto", any(target_env = "nto70", target_env = "nto71"),),
     target_os = "openbsd",
     target_os = "tvos",
     target_os = "watchos",
